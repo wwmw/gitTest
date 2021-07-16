b56e36973490a610ec0ac1d8358dee8e4029e309
2070s <2070s@DESKTOP-F8U3879>
2021-07-16 12:42:26 +0900

    test pc to tablet

---
+++ src/gitPrac/July16_Scanner.java
@@ -0,0 +1,23 @@
+package gitPrac;
+
+import java.util.Scanner;
+
+public class July16_Scanner {
+	
+	public static void main(String[] args) {
+	
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("input your age : ");
+		int x = scan.nextInt();
+
+		System.out.print("input your height : ");
+		int y = scan.nextInt();
+		
+		System.out.println("\n\n--- INFORMATION ---");
+		System.out.println("Age : " + x);
+		System.out.println("Height : " + y);
+		
+		scan.close();
+	}
+}
--- src/gitPrac/PracGit_Cal.java
+++ src/gitPrac/PracGit_Cal.java
@@ -1,12 +1,37 @@
 package gitPrac;
 
+class Calculator{
+	int x;
+	int y;
+	
+	public void setNumber(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public void plus() {
+		System.out.println("x + y = " + (x+y));
+	}
+	public void minus() {
+		System.out.println("x - y = " + (x-y));
+	}
+	public void run() {
+		System.out.println("x : " + x);
+		System.out.println("y : " + y);
+		plus();
+		minus();
+	}
+}
 
-
-public class PracGit_Cal {
+public class PracGit_Cal extends Calculator {
 
 	public static void main(String[] args) {
 		
-
+		Calculator cal = new Calculator();
+		cal.x = 10;
+		cal.y = 20;
+		cal.run();
 	}
 
 }
+
+//클래스 상속의 사용이유?
