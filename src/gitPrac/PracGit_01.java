ee02e7f5da759797423dd112e3b7cdb412fe29f5
wwmw8 <wwmw8@DESKTOP-P2DV5PK>
2021-07-16 11:21:57 +0900

    tablet to pc

--- src/gitPrac/PracGit_01.java
+++ src/gitPrac/PracGit_01.java
@@ -5,6 +5,7 @@ public class PracGit_01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("output test");
+		System.out.println("test from surface");
 
 	}
 
